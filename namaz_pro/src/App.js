0ed9f444b59b0d82966d5a147508ab1d70f00329
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-03-10 02:28:29 -0500

    package fix

--- namaz_pro/src/App.js
+++ namaz_pro/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import {Card} from '@mui/material';
 function App() {
-  return <div className="App">Hello world</div>;
+  return <div className="App">
+    <div>
+      Hello
+      <Card>This should work </Card>
+    </div>
+
+  </div>;
 }
 
 export default App;
